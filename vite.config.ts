743792f0345d1141f6fa1a7a938812cccfa5b5c3
tromb <trombecher@icloud.com>
2024-05-21 22:03:06 +0200

    Switch base path to work for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import {defineConfig} from "vite";
 export default defineConfig({
     esbuild: {
         jsxInject: "import {createElement, Fragment} from 'aena';"
-    }
+    },
+    base: "/mcac/"
 });
